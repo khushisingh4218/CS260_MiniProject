ea677cf0daf99f550e81d7715ffcd6c116e62d16
Shan-Priya96 <94847071+Shan-Priya96@users.noreply.github.com>
2023-04-14 10:52:57 +0530

    finally works

--- studenteligible.php
+++ studenteligible.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
-echo "hi";
 
-$conn = mysqli_connect("localhost", "root", "PASSWORD", "dblab");
+
+$conn = mysqli_connect("localhost", "root", "PASSWORD", "tpc");
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     $mypackage = 0;
     $mycpi = 0;
     $mysem = 0;
     $interest = "";
-    $id=$_SESSION['id'];
-    echo $id;
+    $id= $_SESSION['id'];
 
     $sql = "select * from placements where rollno = '$id'";
     $result = $conn->query($sql);
     while($row = $result->fetch_assoc()){
         if($row["rollno"]==$id){
             $mypackage = (int)$row["ctc"];
-            // $mycpi = (double)$row["ctc"];
-            // $mysem = (int)$row["ctc"];
+            // $mycpi = (double)$row["cpi"];
+            // $mysem = (int)$row["semester"];
         }
     } 
+
 
     
 
@@ -36,30 +36,39 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
         }
 
-    //echo $mypackage." ".$mycpi." ".$mysem;
+
   
-    $sql = "select * from companies where package > $mypackage and min_sem <= $mysem and min_cpi <= $mycpi";
-    //  and min_sem <= $mysem and min_cpi <= $mycpi
+    $sql = "select * from companies ";
+   
     $result = $conn->query($sql);
     while($row = $result->fetch_assoc()){
+
 
             $ccode = $row["ccode"];
             $tv= "select * from skills where ccode = '$ccode'";
             $present = false;
             $res = $conn->query($tv);
             while($r = $res->fetch_assoc()){
-                if($r[$interest]==true){
+
+                if($r[$interest]){
                     $present = true;
-                }
             }
-            if($present){
-                echo $row["ccode"]."   ".$row["cname"]."   ".$row["min_sem"]."   ".$row["min_cpi"]."   ".$row["package"]."   ".$row["mode"]."   ".$row["yor"];
-        echo "<br>";
+            }
+            if($present && $row["package"] > $mypackage){
+               
+                if($row["min_cpi"] <= $mycpi){
+                    if($row["min_sem"] <= $mysem){
+                    echo $row["ccode"]."   ".$row["cname"]."   ".$row["min_sem"]."   ".$row["min_cpi"]."   ".$row["package"]."   ".$row["mode"]."   ".$row["yor"];
+                echo "<br>";
+                    }
+                }
+                
             }
         
             
         
         
     }
+
 }
 ?>
